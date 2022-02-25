30c06df0c6f3261bf179838616b92c5e9a69368d
Romulo Vieira <romulo.vieira777@gmail.com>
2022-02-25 15:06:11 -0300

    Implementing ETL Process with SSIS

---
+++ Section_02_Implementing_ETL_Process_with_SSIS/Files/lookupsqlquery.sql
@@ -0,0 +1,32 @@
+SELECT
+	*
+FROM
+	(SELECT * FROM [dbo].[DimCurrency]) AS refTable  
+WHERE
+	[refTable].[CurrencyAlternateKey] = 'ARS'  
+OR 
+	[refTable].[CurrencyAlternateKey] = 'AUD'  
+OR  
+	[refTable].[CurrencyAlternateKey] = 'BRL'  
+OR  
+	[refTable].[CurrencyAlternateKey] = 'CAD'  
+OR  
+	[refTable].[CurrencyAlternateKey] = 'CNY'  
+OR  
+	[refTable].[CurrencyAlternateKey] = 'DEM'  
+OR  
+	[refTable].[CurrencyAlternateKey] = 'EUR'  
+OR  
+	[refTable].[CurrencyAlternateKey] = 'FRF'  
+OR  
+	[refTable].[CurrencyAlternateKey] = 'GBP'  
+OR  
+	[refTable].[CurrencyAlternateKey] = 'JPY'  
+OR  
+	[refTable].[CurrencyAlternateKey] = 'MXN'  
+OR  
+	[refTable].[CurrencyAlternateKey] = 'SAR'  
+OR  
+	[refTable].[CurrencyAlternateKey] = 'USD'  
+OR  
+	[refTable].[CurrencyAlternateKey] = 'VEB'
